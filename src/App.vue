4b08da2b64ffcf5dac28456a55ce876326b9d4cd
Vybhav Upadhyayula <vupadhyayula@lbl.gov>
2019-06-28 13:18:58 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,27 @@
     <b-navbar-brand href="#">XiCAM Plugins</b-navbar-brand>
     <b-collapse is-nav id="nav_collapse">
     </b-collapse>
+     <b-navbar-nav class="ml-auto">
+      <b-nav-form>
+        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+      </b-nav-form>
+      <b-nav-item-dropdown text="Lang" right>
+        <b-dropdown-item href="#">EN</b-dropdown-item>
+        <b-dropdown-item href="#">ES</b-dropdown-item>
+        <b-dropdown-item href="#">RU</b-dropdown-item>
+        <b-dropdown-item href="#">FA</b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item-dropdown right>
+        <template slot="button-content">
+          <em>User</em>
+        </template>
+        <b-dropdown-item href="#">Profile</b-dropdown-item>
+        <b-dropdown-item href="#">Signout</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-navbar-nav>
     </b-navbar>
+    
     <b-container>
       <b-row align-h="center" class="mt-5">
         <b-col cols="12">
@@ -25,7 +45,7 @@
           <b-card class="p-3">
             <h3 class="mb-2"></h3>
               <div>
-                <b-dropdown id="dropdown-1" text="Plugin Information" class="m-md-1">
+                  <b-dropdown id="dropdown-1" text="Plugin Information" class="m-md-1">
                   <b-dropdown-item>IPython</b-dropdown-item>
                   <b-dropdown-item>Log</b-dropdown-item>
                   <b-dropdown-item>Thread Monitor</b-dropdown-item>
@@ -36,6 +56,7 @@
         </b-col>
       </b-row>
     </b-container>
+    <br><br><br><br>
     <b-container>
       <b-row align-h="center" class="mt-5">
         <b-col cols="12">
@@ -91,7 +112,9 @@
           </b-card>
         </b-col>
       </b-row>
-    </b-container>            
+    </b-container> 
+    <br><br>  
+             
   </div>
 </template>
 
