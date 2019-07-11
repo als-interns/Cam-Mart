84569a3879fd3fba2c5295bac3faf7a9b965c63c
NYashchuk <51764820+NYashchuk@users.noreply.github.com>
2019-07-11 11:55:09 -0700

    Update main.js

--- src(nik)/main.js
+++ src(nik)/main.js
@@ -4,14 +4,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+Vue.use(BootstrapVue)
 
 new Vue({
   el: '#app',
   router,
+  render: h => h(App),
   components: { App },
   template: '<App/>'
 })
