160b38471086e7cb7b7a89486de6662304ec7030
NYashchuk <51764820+NYashchuk@users.noreply.github.com>
2019-07-23 12:31:50 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 		<b-nav-item href="#/home">Home</b-nav-item>
 		<b-nav-item href="#/Login">Login</b-nav-item>
 		<b-nav-item href="#/Submit">Submit a Plugin</b-nav-item>
+		<b-nav-item href="#/Profile">Profile</b-nav-item>
 		</b-navbar-nav>
 	</b-navbar>
 	<br>
@@ -17,16 +18,12 @@
 
 import axios from 'axios'
 /*
-showlogin - boolean to show login
-page - pages (not working, just a template)
-ispageone - boolean by default is true to prevent from going into negative pages
-loggedin - boolean
+
 */
 export default {
   name: 'Home',
   data () {
     return {
-	  indplugin: {},
 	  loggedin: false,
       user: '',
 	  navfields: [
@@ -45,14 +42,7 @@ export default {
 		{
 		text: 'Cam-mart', 
 		}
-	  ],
-	  submit: function () {
-		if (!(this.loggedin)){
-			alert("Log in or create an account")
-		}else{
-			this.$bvModal.show("submit")
-		}
-	  }
+	  ]
     }
   }
 }
