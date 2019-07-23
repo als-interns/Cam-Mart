728af03b709ffd330c58301ce04acc358b142dc0
NYashchuk <51764820+NYashchuk@users.noreply.github.com>
2019-07-23 12:33:58 -0700

    Add files via upload

---
+++ src/components/Profile.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="profile">
+    <router-view/>
+	
+	</div>
+</template>
+
+<script>
+export default{
+	data() {
+		return{
+		
+		}
+	},
+	methods: {
+	
+	}
+}
+</script>
+
+<style>
+
+</style>
