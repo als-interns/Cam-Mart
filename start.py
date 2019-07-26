8d29bdc2fc2d7659d6cefb1f782834d5b7ab8d7c
NYashchuk <51764820+NYashchuk@users.noreply.github.com>
2019-07-26 15:25:57 -0700

    Update start.py

--- start.py
+++ start.py
@@ -2,11 +2,11 @@ from eve import Eve
 from eve.auth import BasicAuth
 from eve.io.mongo import Validator
 from flask import request
+import json
 import bcrypt
 from redis import StrictRedis
 
 class MyBasicAuth(BasicAuth):
-
     def check_auth(self, username, password, allowed_roles, resource, method):
         if method == 'GET' and not resource == 'accounts':
             return True
@@ -19,13 +19,28 @@ class MyBasicAuth(BasicAuth):
                 return False
         elif resource == 'accounts' and method == 'POST':
             return True
+        elif resource == 'login' and method == 'POST':
+            return True
         else:
             return False
 
 
-
 app = Eve(auth=MyBasicAuth, settings='settings.py')
 
+
+@app.route('/login', methods=['POST'])
+def login():
+    info = request.data
+    #info = info.decode('utf-8') <-- converts to str, not necesary
+    info = json.loads(info)
+    user = app.data.driver.db['accounts']
+    user = user.find_one(info)
+    if user:
+        print(info['username'] + ' has logged in')
+        return 'ok'
+    else:
+        print('someone failedto login')
+        return 'fail'
 
 
 if __name__ == '__main__':
