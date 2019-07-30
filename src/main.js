cfca9c8506d7179f71ecd963c5b2b5964eb02217
NYashchuk <51764820+NYashchuk@users.noreply.github.com>
2019-07-30 13:16:07 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,13 +6,32 @@ import App from './App'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
+import Vuex from 'vuex'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 Vue.use(BootstrapVue)
+Vue.use(Vuex)
+
+const store =  new Vuex.Store({
+	state: {
+		user: 'none',
+		token: ''
+	},
+	mutations: {
+		newUser(state, theuser) {
+			state.user = theuser
+		},
+		newToken(state, token){
+			state.token = token
+		}
+	}
+})
+//Vue.prototype.$user = {username: 'none'}
 
 new Vue({
   el: '#app',
+  store,
   router,
   render: h => h(App),
   components: { App },
